4107a3c455cac5b03105a2b87d9d3b31b62827bb
Mart510 <martynandrewbillings@gmail.com>
2024-05-13 21:44:57 +0100

    another one

--- .firebase/holibobber/functions/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.DWHPqe8l.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.D16DZW7K.js"];
-export const stylesheets = ["_app/immutable/assets/0.Tdhp2XSG.css"];
-export const fonts = [];
